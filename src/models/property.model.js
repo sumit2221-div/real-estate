222b06f5d99bde40eb928e02991a993154af5e7c
sumit212-dev <132038818+sumit2221-div@users.noreply.github.com>
2024-08-14 12:18:49 +0530

    make some chnages

--- src/models/property.model.js
+++ src/models/property.model.js
@@ -1,45 +1,50 @@
-import mongoose , {Schema} from "mongoose";
+import mongoose, { Schema } from "mongoose";
 
 const propertySchema = new Schema(
-    {
+  {
+    owner: {
+      type: Schema.Types.ObjectId,
+      ref: "User",
+    },
+    name: {
+      type: String,
+      required: true,
+      unique: true,
+      lowercase: true,
+      trim: true,
+      index: true,
+    },
+    type: {
+      type: String,
+      enum: ['flat', 'building', 'plot', 'bungalow'],
+      required: true,
+    },
+    size: {
+      type: String,
+      required: true,
+    },
+    cost: {
+      type: String,
+      required: true,
+    },
+    status: {
+      type: String,
+      enum: ['buy', 'rent'],
+      required: true,
+    },
+    photos: [
+      {
+        type: String,
+      },
+    ],
+    address: {
+      type: Schema.Types.ObjectId,
+      ref: "Address",
+    },
+  },
+  {
+    timestamps: true,
+  }
+);
 
-        owner : {
-          type:Schema.Types.ObjectId,
-      ref : "User"
-
-        },
-        name  : {
-            type: String,
-            required: true,
-            unique: true,
-            lowercase: true,
-            trim: true, 
-            index: true
-
-        }, 
-        type : {
-            type: String, enum: ['flat', 'building', 'plot', 'bungalow'], required: true ,
-        }, 
-        size : {
-            type : String,
-            required : true
-        }, 
-        cost : {
-            type : String,
-            required : true
-        }, 
-        photos  : [
-             {type : String, }
-        ], 
-        address : {
-            type : Schema.Types.ObjectId,
-            ref : 'Address'
-        }
-         
-
-    }, {
-        Timestamp : true
-    }
-)
-
-export const Property = mongoose.model('Property', propertySchema)
+export const Property = mongoose.model('Property', propertySchema);
